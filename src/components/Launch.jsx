8913390435b79756282124d6e53b1664fb3d2626
Kira Corbett <kiracorbett00@gmail.com>
2021-10-16 01:46:00 -0700

    Added clickable to expand and close list

--- src/components/Launch.jsx
+++ src/components/Launch.jsx
@@ -1,7 +1,7 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { Accordion, Icon } from 'semantic-ui-react';
 
 class Launch extends Component {
-
   constructor(props) {
     super(); 
     this.state = { showDetails: false };
@@ -21,6 +21,7 @@ class Launch extends Component {
     return (
       <li>
         <button onClick={this.showDetails.bind(null, showDetails)}><h2> { launch.mission_name } </h2></button>
+        
         { this.state.showDetails && (
           <ul>
             <div>Rocket ID: { launch.rocket.rocket_id } </div>
