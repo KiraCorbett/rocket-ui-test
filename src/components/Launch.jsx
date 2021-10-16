f6c61edabfde4ed5f37471fc94c863b6e8bce6a5
Kira Corbett <kiracorbett00@gmail.com>
2021-10-16 09:24:43 -0700

    Handle page clicking to close dropdowns

--- src/components/Launch.jsx
+++ src/components/Launch.jsx
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
-import { Accordion, Icon } from 'semantic-ui-react';
 
 class Launch extends Component {
-  constructor(props) {
-    super(); 
-    this.state = { showDetails: false };
-  }
+  container = React.createRef();
+  state = {
+    showDetails: false,
+  };
 
-  showDetails = (bool) => {
-    this.setState({
-      showDetails: !bool
+  handleClick = () => {
+    this.setState((state) => {
+      return {
+        showDetails: !state.showDetails,
+      };
     });
+  };
+
+  handlePageClick = (event) => {
+    if (
+      this.container.current &&
+      !this.container.current.contains(event.target)
+    ) {
+      this.setState({
+        showDetails: false,
+      });
+    }
+  };
+
+  // React Ref tutorial source: https://codedaily.io/tutorials/Create-a-Dropdown-in-React-that-Closes-When-the-Body-is-Clicked
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handlePageClick);
+  } 
+
+  // React Ref tutorial source: https://codedaily.io/tutorials/Create-a-Dropdown-in-React-that-Closes-When-the-Body-is-Clicked
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handlePageClick);
   }
 
   render() {
-
     let launch = this.props.launch;
-    const showDetails = this.state.showDetails;
 
     return (
-      <li>
-        <button onClick={this.showDetails.bind(null, showDetails)}><h2> { launch.mission_name } </h2></button>
-        
-        { this.state.showDetails && (
-          <ul>
-            <div>Rocket ID: { launch.rocket.rocket_id } </div>
-            <div>Cost: { launch.cost } </div>
-            <div>Details: { launch.details } </div>
-          </ul>
-        ) }
-      </li>
+      <ul>
+        <div className="container" ref={this.container}>
+          <button onClick={this.handleClick}><h2> { launch.mission_name } </h2></button>
+          {this.state.showDetails && (
+            <ul>
+              <div>Rocket ID: { launch.rocket.rocket_id } </div>
+              <div>Cost: { launch.cost } </div>
+              <div>Details: { launch.details } </div>
+            </ul>
+          )}
+        </div>
+      </ul>
     );
   }
 }
