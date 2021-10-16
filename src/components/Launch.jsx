7238dfd275d87dee21c08963ca9570d9d690f92a
Kira Corbett <kiracorbett00@gmail.com>
2021-10-16 00:52:48 -0700

    Converted launch title to button

--- src/components/Launch.jsx
+++ src/components/Launch.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 
 class Launch extends Component {
-
-  // test
-  showMessage() {
-    alert('Hello!');
-  }
-
   render() {
 
     let launch = this.props.launch;
 
     return (
       <li>
-        <h2> { launch.mission_name } </h2>
+        <button><h2> { launch.mission_name } </h2></button>
         <div> Flight Number: { launch.flight_number } </div>
         <div> Rocket ID: { launch.rocket.rocket_id } </div>
         <div> Cost per Launch: { launch.cost } </div>
         <div> Description: { launch.details } </div>
-        <button onClick={this.showMessage}>Button</button>
       </li>
     );
   }
